b75016e610e72fc9a14c1adabea77145d3d9e54b
Jisun Park <fpdldk912@gmail.com>
2020-04-10 18:20:38 +0900

    modify views from function base to class-based

--- snippets/views.py
+++ snippets/views.py
@@ -1,47 +1,50 @@
-from rest_framework import status 
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
 
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
+class SnippetList(APIView):
     """
     List all code snippets, or create new snippet.
     """
-    if request.method == 'GET': # snippets 모두 불러오기
+    def get(self, request, format=None):
         snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True) # queryset을 serialize 하기
-        return Response(serializer.data) # json으로 return
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
 
-    elif request.method == 'POST': # snippet 새로 만들기
-        serializer = SnippetSerializer(data=request.data) # request.data를 통해 어떤 데이터도 가져올 수 있음
-        if serializer.is_valid(): # 성공
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED) # 명확한 status reponse 가능
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # 실패
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None):
+class SnippetDetail(APIView):
     """
     Retrieve, update or delete a code snippet.
     """
-    try:
-        snippet = Snippet.objects.get(pk=pk) # 특정 snippet 가져오기
-    except Snippet.DoesNotExist: # 일치하는 snippet이 없을 경우
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
     
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
-    
-    elif request.method == 'PUT':
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
+        
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
